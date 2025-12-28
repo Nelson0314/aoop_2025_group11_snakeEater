e27fbf5fab0a69a232f984337bdc4cf5b0ca24c0
Nelson0314 <nelson.weng20@gmail.com>
2025-12-28 21:05:57 +0800

    feat: increase computer snake count in non-learn mode from 35 to 55

--- game.py
+++ game.py
@@ -71,7 +71,7 @@ class GAME():
 
         # Create Computer Snakes
         # In learn mode, we might want MORE snakes to speed up training?
-        count = 50 if self.mode == 'learn' else 35 # More snakes in learn mode
+        count = 50 if self.mode == 'learn' else 31 # More snakes in learn mode
         
         for i in range(count):
             cx = random.randint(100, MAP_WIDTH - 100)
